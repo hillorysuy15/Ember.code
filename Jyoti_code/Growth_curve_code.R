fadda1a3a90f45f3c6f1d53c9a2528743190d6d9
hillorysuy15 <suy8096@vandals.uidaho.edu>
2026-03-17 11:27:59 -0700

    new:

--- Jyoti_code/Growth_curve_code.R
+++ Jyoti_code/Growth_curve_code.R
@@ -1,4 +1,4 @@
-### Loading library and files
+### Loading library and files ----
 
 library(readxl)
 library(tidyverse)
@@ -6,20 +6,21 @@ library(dplyr)
 library(lubridate)
 library(ggplot2)
 library(patchwork)
-A <- read_excel("~/Documents/BCB5200/Isolate A.xlsx")
 
+
+A <- read_excel("~/Documents/BCB5200/Isolate A.xlsx")
 B <- read_excel("~/Documents/BCB5200/Isolate B .xlsx")
 G <- read_excel("~/Documents/BCB5200/Isolate G .xlsx")
 H <- read_excel("~/Documents/BCB5200/Isolate H .xlsx")
 
-## Bind rows to one dataset
+## Bind rows to one dataset ----
 A$Isolate <- "A"
 B$Isolate <- "B"
 G$Isolate <- "G"
 H$Isolate <- "H"
-dat <- bind_rows(A, B, G, H)
+dat <- bind_rows(A, B, G, H) 
 
-# Without log scale graft
+# Without log scale graph ----
 
 p1 <- ggplot(dat, aes(x = Time_min, y = Mean_WT_Norm, color = Isolate)) +
   geom_point() +
@@ -39,7 +40,7 @@ p1 <- ggplot(dat, aes(x = Time_min, y = Mean_WT_Norm, color = Isolate)) +
                                      face = "bold"))
 
 
-## Log scale Graph
+## Log scale Graph ----
 
 
 p2 <- ggplot(dat, aes(x = Time_min, y = Mean_WT_Norm, color = Strain)) +
@@ -64,6 +65,7 @@ p2 <- ggplot(dat, aes(x = Time_min, y = Mean_WT_Norm, color = Strain)) +
 
 p1 + p2
 
+## Save it to your laptop ----
 
 ggsave(p1, file = "jyoti_code/plots/growth_curve.png", width = 12, height = 9, unit = "in", dpi = 400)
 ggsave(p2, file = "jyoti_code/plots/growth_curve_log.png", width = 12, height = 9, unit = "in", dpi = 400)
